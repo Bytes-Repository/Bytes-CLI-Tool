3cb24f85fce204e71b1bd6cb4ee2f27fbb1f5375
michal92299 <chudybyczek2137@gmail.com>
2025-11-29 09:22:00 +0100

    Update download.cr

--- src/download.cr
+++ src/download.cr
@@ -1,55 +1,46 @@
 require "http/client"
 require "colorize"
 require "uri"
-
-def get_final_url(url : String) : String
+def download_with_progress(url : String, dest : String) : Nil
   headers = HTTP::Headers{"User-Agent" => "bytes-io-cli/0.5 (Crystal)"}
   max_redirects = 10
-  parsed_url = URI.parse(url)
+  current_url = url
+  file_name = File.basename(url)
   while max_redirects > 0
+    parsed_url = URI.parse(current_url)
     client = HTTP::Client.new(parsed_url)
-    response = client.head(parsed_url.path || "/", headers: headers)
-    if response.status_code == 200
-      return url
-    elsif response.status_code.in?(301, 302, 303, 307, 308)
-      location = response.headers["Location"]?
-      if location
-        url = location.starts_with?("http") ? location : parsed_url.resolve(location).to_s
-        parsed_url = URI.parse(url)
-        max_redirects -= 1
+    client.get(parsed_url.path || "/", headers: headers) do |response|
+      if response.status_code == 200
+        total = response.headers["Content-Length"]?.try(&.to_i64) || 0_i64
+        read = 0_i64
+        File.open(dest, "wb") do |file|
+          buffer = Bytes.new(4096)
+          while (bytes_read = response.body_io.read(buffer)) > 0
+            file.write(buffer[0...bytes_read])
+            read += bytes_read
+            if total > 0
+              percent = (read.to_f / total * 100).round(2)
+              print "\r#{info_style("Downloading #{file_name}...")} #{percent}% of #{total} bytes".ljust(80)
+            else
+              print "\r#{info_style("Downloading #{file_name}...")} #{read} bytes downloaded".ljust(80)
+            end
+            STDOUT.flush
+          end
+        end
+        puts # New line after progress
+        return
+      elsif response.status_code.in?(301, 302, 303, 307, 308)
+        location = response.headers["Location"]?
+        if location
+          current_url = location.starts_with?("http") ? location : parsed_url.resolve(location).to_s
+          max_redirects -= 1
+        else
+          raise "Redirect without location"
+        end
       else
-        raise "Redirect without location"
+        raise "Bad status: #{response.status_code}"
       end
-    else
-      raise "Bad status: #{response.status_code} for HEAD"
     end
   end
   raise "Too many redirects"
-end
-
-def download_with_progress(url : String, dest : String) : Nil
-  final_url = get_final_url(url)
-  headers = HTTP::Headers{"User-Agent" => "bytes-io-cli/0.5 (Crystal)"}
-  HTTP::Client.get(final_url, headers: headers) do |response|
-    if response.status_code != 200
-      raise "Bad status: #{response.status_code}"
-    end
-    total = response.headers["Content-Length"]?.try(&.to_i64) || 0_i64
-    read = 0_i64
-    File.open(dest, "wb") do |file|
-      buffer = Bytes.new(4096)
-      while (bytes_read = response.body_io.read(buffer)) > 0
-        file.write(buffer[0...bytes_read])
-        read += bytes_read
-        if total > 0
-          percent = (read.to_f / total * 100).round(2)
-          print "\r#{info_style("Downloading #{File.basename(url)}...")} #{percent}% of #{total} bytes".ljust(80)
-        else
-          print "\r#{info_style("Downloading #{File.basename(url)}...")} #{read} bytes downloaded".ljust(80)
-        end
-        STDOUT.flush
-      end
-    end
-    puts # New line after progress
-  end
 end
